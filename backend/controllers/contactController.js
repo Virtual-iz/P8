60d5e419eac1b0655e7d9419f1ffc7b767f00591
Isazez <isazez@etik.com>
2026-05-14 15:55:49 +0200

    Passage de IPV6 à IPV4

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -4,6 +4,7 @@ const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: parseInt(process.env.SMTP_PORT, 10) || 587,
   secure: false,
+  family: 4,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
